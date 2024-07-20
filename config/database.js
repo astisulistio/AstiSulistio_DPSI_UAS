c959f9a071d782a9cb41ea326675d9d852c26ce7
astisulistio <asti02sulistio@gmail.com>
2024-07-21 00:25:36 +0700

    Fix Sequelize configuration

--- config/database.js
+++ config/database.js
@@ -4,16 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  port: 3306
+  port: process.env.DB_PORT || 3306, // Gunakan port dari variabel lingkungan atau default ke 3306
 });
 
-module.exports = {
-  development: {
-    username: 'sql12721016',
-    password: 'VAcFzfRu5F',
-    database: 'sql12721016',
-    host: 'sql12.freesqldatabase.com',
-    port: 3306,
-    dialect: 'mysql',
-  },
-};
+module.exports = sequelize;
