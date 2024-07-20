5174c08b2a39258eedf35dd62eede719db456831
astisulistio <asti02sulistio@gmail.com>
2024-07-21 00:17:22 +0700

    Update database configuration for Heroku

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  port: 3306
 });
 
 module.exports = sequelize;
